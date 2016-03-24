39ec4365af37268474e1b2c43ec8ae5311610c9a
Ray Mitchell <rmitchell@fairwaytech.com>
2016-03-24 00:17:40 -0700

    Got euler1 working (in hello-world project).

---
+++ hello-world/euler1.ts
@@ -0,0 +1,6 @@
+import _ = require("underscore");
+
+let result = _.range(1000)
+  .filter((a) => { return a % 3 == 0 || a % 5 == 0; })
+  .reduce((a, b) => a + b);
+console.log(result);  // 233168
--- hello-world/typings/main.d.ts
+++ hello-world/typings/main.d.ts
@@ -1,1 +1,2 @@
 /// <reference path="main/ambient/node/index.d.ts" />
+/// <reference path="main/ambient/underscore/index.d.ts" />
